2b94f922b590a38cb431cc2914f5fe969d05cd7e
cddlslt <oatesmi.74@gmail.com>
2018-05-10 11:32:39 -0500

    errors fixed, need to add confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510130917) do
+ActiveRecord::Schema.define(version: 20180510161619) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
