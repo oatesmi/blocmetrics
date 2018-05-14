4d94febc848b8c1b865c37fef2d13ac22ab2a488
cddlslt <oatesmi.74@gmail.com>
2018-05-14 04:34:48 -0500

    api controller complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :registered_applications
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 end
